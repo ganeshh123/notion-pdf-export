5f7f90f584bc046100578c158413fe84334af5d4
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2020-08-03 17:34:44 +0100

    folder_deletion?

--- index.js
+++ index.js
@@ -3,11 +3,54 @@ let markdownpdf = require('markdown-pdf')
 let fileSystem = require('fs')
 let path = require('path')
 
+/*Function to remove markdown files after conversion */
+let deleteFiles = (filesToDelete, callback) => {
+    if (filesToDelete.length==0) callback();
+    else {
+       var f = filesToDelete.pop();
+       fileSystem.unlink(f, function(err){
+          if (err) callback(err);
+          else {
+             console.log(f + ' deleted.');
+             deleteFiles(filesToDelete, callback);
+          }
+       });
+    }
+ }
+
+
+ const removeDir = function(path) {
+    if (fileSystem.existsSync(path)) {
+      /*Delete Markdown Folders with assets */
+        let files = fileSystem.readdirSync(path).filter((file) => {
+            return file != 'node_modules'
+                && file != 'pdfs'
+                && file != '.git'
+                
+        }
+        )
+        console.log(files)
+  
+      if (files.length > 0) {
+        files.forEach(function(filename) {
+          if (fileSystem.statSync(path + "/" + filename).isDirectory()) {
+            removeDir(path + "/" + filename)
+          } else {
+            fileSystem.unlinkSync(path + "/" + filename)
+          }
+        })
+        fileSystem.rmdirSync(path)
+      } else {
+        fileSystem.rmdirSync(path)
+      }
+    }
+  }
+
 /* Create an Array of Markdown Files to be Converted */
 let markdownFiles = []
-fileSystem.readdirSync(process.cwd() + '/markdowns/').forEach(file => {
+fileSystem.readdirSync(__dirname).forEach(file => {
     if(file.includes('.md')){
-        markdownFiles.push(process.cwd() + '/markdowns/' + file)
+        markdownFiles.push(file)
     }
 })
 console.log('Files to be converted :')
@@ -22,23 +65,27 @@ markdownpdf().concat.from(markdownFiles).to(bookPath, function () {
     /*Export Individual Documents as PDF */
     console.log('Exporting Pages Individually')
     let pdfDocs = []
-    fileSystem.readdirSync(path.join(process.cwd(), 'markdowns')).forEach(fileName => {
+    fileSystem.readdirSync(__dirname).forEach(fileName => {
         if(fileName.includes('.md')){
             let pdfFileName = fileName.replace('.md', '.pdf')
-            console.log(pdfFileName)
             pdfDocs.push(path.join(process.cwd(), 'pdfs', 'pages', pdfFileName))
         }
     })
-    console.log(pdfDocs)
     markdownpdf().from(markdownFiles).to(pdfDocs, function () {
-        console.log('Converting')
         pdfDocs.forEach(function (d) { console.log("Created", d) })
 
-        /* Only exit when user presses a key */
-        console.log('Press any key to exit');
-        process.stdin.setRawMode(true);
-        process.stdin.resume();
-        process.stdin.on('data', process.exit.bind(process, 0));
+
+        /*Delete Original Markdown Files and Folders */
+        console.log('Deleting Original Markdown Files')
+        let filesToDelete = markdownFiles.slice()
+        deleteFiles(filesToDelete, () => {
+
+            
+            removeDir(process.cwd())
+        })
+
+
+        
     })
 
 })
