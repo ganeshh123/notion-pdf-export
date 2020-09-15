0ef29b12dfc0a8d6087c1aef8f480e5c2ebc8115
Ganesh H <43777439+ganeshh123@users.noreply.github.com>
2020-09-15 21:46:50 +0100

    Update build_win32.bat

--- build_win32.bat
+++ build_win32.bat
@@ -1,6 +1,9 @@
 call npm install
 robocopy node_modules dist/win32/node_modules /E
-robocopy utils dist/win32/utils /E
+robocopy utils/wkhtmltopdf dist/win32/utils/wkhtmltopdf /E
 copy ".\LICENSE" ".\dist\win32\LICENSE"
 del ".\dist\win32\node_modules\phantomjs-prebuilt\lib\phantom\bin\phantomjs.exe" /f /q
+del ".\dist\win32\utils\wkhtmltopdf\wkhtmltopdf" /f /q
+del ".\dist\win32\utils\wkhtmltopdf\wkhtmltox.dll" /f /q
+del ".\dist\win32\utils\wkhtmltopdf\libwkhtmltox.a" /f /q
 nexe . --target win32-x86-10.13.0 -o ./dist/win32/notion-pdf-export.exe
